77dd1ed7fed57d29e3417715c132ea19f2430acb
vipin pandey <106507638+vipinpandey1804@users.noreply.github.com>
2024-09-10 13:58:57 +0530

    change name

--- friendship/apps.py
+++ friendship/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class FriendshipConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
-    name = "Mobile-Django-Boilerplate-2.0-Friends-Module"
+    name = "friendship"
     label = "friendship"
